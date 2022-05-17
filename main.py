9feffc999959a3eff70df349773c2bda0f44c1a8
HuguesFARTH <matrix83gamer@gmail.com>
2022-05-17 08:33:02 +0200

    2.1

--- main.py
+++ main.py
@@ -38,23 +38,10 @@ def init_data():
 
 def run(window):
     # boucle d'affichage
-    last = glfw.get_time()
-    count = 0
     while not glfw.window_should_close(window):
-        current = glfw.get_time()
-        if current - last > 1:
-            last = current
-            print(count)
-            count = 0
-        else:
-            count += 1
-        
-        GL.glClearColor(current%1, 0.5, 0.9, 1.0)
         # nettoyage de la fenêtre : fond et profondeur
         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
-
         #  l'affichage se fera ici
-
         # changement de buffer d'affichage pour éviter un effet de scintillement
         glfw.swap_buffers(window)
         # gestion des évènements
@@ -72,6 +59,43 @@ def main():
     init_data()
     run(window)
     glfw.terminate()
+
+def compile_shader(shader_content, shader_type):
+# compilation d'un shader donnée selon son type
+    shader_id = GL.glCreateShader(shader_type)
+    GL.glShaderSource(shader_id, shader_content)
+    GL.glCompileShader(shader_id)
+    success = GL.glGetShaderiv(shader_id, GL.GL_COMPILE_STATUS)
+    if not success:
+        log = GL.glGetShaderInfoLog(shader_id).decode('ascii')
+        print(f'{25*"-"}\nError compiling shader: \n\
+        {shader_content}\n{5*"-"}\n{log}\n{25*"-"}')
+    return shader_id
+
+def create_program( vertex_source, fragment_source):
+    # creation d'un programme gpu
+    vs_id = compile_shader(vertex_source, GL.GL_VERTEX_SHADER)
+    fs_id = compile_shader(fragment_source, GL.GL_FRAGMENT_SHADER)
+    if vs_id and fs_id:
+        program_id = GL.glCreateProgram()
+        GL.glAttachShader(program_id, vs_id)
+        GL.glAttachShader(program_id, fs_id)
+        GL.glLinkProgram(program_id)
+        success = GL.glGetProgramiv(program_id, GL.GL_LINK_STATUS)
+    if not success:
+        log = GL.glGetProgramInfoLog(program_id).decode('ascii')
+        print(f'{25*"-"}\nError linking program:\n{log}\n{25*"-"}')
+        GL.glDeleteShader(vs_id)
+        GL.glDeleteShader(fs_id)
+    return program_id
+
+def create_program_from_file(vs_file, fs_file):
+    # creation d'un programme gpu `a partir de fichiers
+    vs_content = open(vs_file, 'r').read() if os.path.exists(vs_file)\
+    else print(f'{25*"-"}\nError reading file:\n{vs_file}\n{25*"-"}')
+    fs_content = open(fs_file, 'r').read() if os.path.exists(fs_file)\
+    else print(f'{25*"-"}\nError reading file:\n{fs_file}\n{25*"-"}')
+    return create_program(vs_content, fs_content)
 
 if __name__ == '__main__':
     main()
